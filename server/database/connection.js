7a3051a7bf8fb3b142f59b60f17afb0b9afeaff2
khadijahnclark <khadijahnclark@gmail.com>
2024-12-10 16:29:35 -0600

    installing axios

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,5 +1,11 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
+const api_key = process.env.API_KEY ||'';
+
+
+const steam_app_url = `https://api.steampowered.com/ISteamApps/GetAppList/v2/`
+
+
 const uri = process.env.ATLAS_URI || "";
 const client = new MongoClient(uri, {
   serverApi: {
@@ -10,17 +16,44 @@ const client = new MongoClient(uri, {
 });
 
 try {
-  // Connect the client to the server
-  await client.connect();
-  // Send a ping to confirm a successful connection
-  await client.db("admin").command({ ping: 1 });
-  console.log(
-   "Pinged your deployment. You successfully connected to MongoDB!"
-  );
-} catch(err) {
-  console.error(err);
-}
-
-let db = client.db("overnout");
-
+    // Connect the client to the server
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+     "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch(err) {
+    console.error(err);
+  }
+  
+  let db = client.db("overnout");
+  
+  (async() =>{
+    try{
+      const gameData = db.collection('gamedata');
+      let gameDocument = [];
+  
+      const response = await axios.get(steam_app_url);
+      const applists = response.data.applist.apps;
+  
+      for(let i = 0; i < applists.length && gameDocument.length < 150; i++){
+        const app = applists[i];
+          gameDocument.push({
+            "appId": app.appid,
+            "name": app.name
+          });
+      }
+  
+      if(gameDocument.length != 0 ){
+        const p = await gameData.insertMany(gameDocument);
+        console.log(`${p.insertedCount} documents were inserted`)
+      }else{
+        console.log(`no documents to insert, skipping insertMany`)
+      }
+    } catch(err){
+      console.log(err.message);
+    }
+  })();
+  
 export default db;
